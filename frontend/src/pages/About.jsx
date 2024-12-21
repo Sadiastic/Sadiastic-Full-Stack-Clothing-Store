77d0da85145234102eb52b8b30969ee930e85598
Sadiastic <sadiastic@gmail.com>
2024-12-21 23:49:35 +0500

    first commit of my MERN Stack Web Application for DBS Course

---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/assets'
+import NewsletterBox from '../components/NewsletterBox'
+
+const About = () => {
+  return (
+    <div>
+
+      <div className='text-2xl text-center pt-8 border-t'>
+          <Title text1={'ABOUT'} text2={'US'} />
+      </div>
+
+      <div className='my-10 flex flex-col md:flex-row gap-16'>
+          <img className='w-full md:max-w-[450px]' src={assets.about_img} alt="" />
+          <div className='flex flex-col justify-center gap-6 md:w-2/4 text-gray-600'>
+              <p align = 'justify'>I’m Sadia Malik, a Computer Engineering student at COMSATS University Islamabad, Attock Campus, with a passion for technology, entrepreneurship, and design. I specialize in web development and database management, constantly seeking innovative solutions and creating user-friendly applications.</p>
+              <p align = 'justify'>I am also the founder of Sadiastic, an online clothing store that offers stylish and high-quality apparel for everyone. Since our inception, we've worked tirelessly to curate a diverse selection of high-quality products that cater to every taste and preference. In, we offer an extensive collection sourced from trusted brands and suppliers.</p>
+              <b className='text-gray-800'>Our Mission</b>
+              <p align = 'justify'>Our mission is to provide accessible and affordable fashion while ensuring a sustainable and exceptional customer experience. This project was built for my DBMS course at university using the MERN Stack, combining my skills in technology and my entrepreneurial drive.</p>
+          </div>
+      </div>
+
+      <div className=' text-xl py-4'>
+          <Title text1={'WHY'} text2={'CHOOSE US'} />
+      </div>
+
+      <div className='flex flex-col md:flex-row text-sm mb-20'>
+          <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+            <b>Quality Assurance:</b>
+            <p className=' text-gray-600'>We meticulously select and vet each product to ensure it meets our stringent quality standards.</p>
+          </div>
+          <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+            <b>Convenience:</b>
+            <p className=' text-gray-600'>With our user-friendly interface and hassle-free ordering process, shopping has never been easier.</p>
+          </div>
+          <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+            <b>Exceptional Customer Service:</b>
+            <p className=' text-gray-600'>Our team of dedicated professionals is here to assist you the way, ensuring your satisfaction is our top priority.</p>
+          </div>
+      </div>
+
+      <NewsletterBox/>
+      
+    </div>
+  )
+}
+
+export default About
